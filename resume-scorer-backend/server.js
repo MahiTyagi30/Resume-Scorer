cd71cd66ef3e1d94309b1c42a25907dd3b496871
Mahi Tyagi <mahityagi2003@gmail.com>
2024-09-27 13:33:24 +0530

    removed extra code

--- resume-scorer-backend/server.js
+++ resume-scorer-backend/server.js
@@ -7,8 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(express.json()); // Add this to handle JSON data
-
+app.use(express.json()); 
 // Set up multer to handle file uploads in memory
 const upload = multer({
   storage: multer.memoryStorage(),
